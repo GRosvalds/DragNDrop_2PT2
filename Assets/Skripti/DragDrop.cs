3c3fc15d2747eafc217757785f9dcfa137136352
Gabriels Rosvalds <gabriels.rosvalds@gmail.com>
2023-05-24 12:18:40 +0300

    Pievienotas bildes, fons, izveidoti DragDrop un Objekti scripti

---
+++ Assets/Skripti/DragDrop.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDrop : MonoBehaviour,
+	IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public Objekti objektuSkript;
+    private CanvasGroup kanvasGroup;
+    private RectTransform velkObjRectTransf;
+
+    void Start()
+    {
+        kanvasGroup = GetComponent<CanvasGroup>();
+        velkObjRectTransf = GetComponent<RectTransform>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    
+	
+	
+}
